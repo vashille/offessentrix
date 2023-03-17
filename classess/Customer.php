d5ca28f2f6c56b5f12d40eaf937b7825e25e17cc
vashille <127309061+vashille@users.noreply.github.com>
2023-03-17 19:36:04 +0800

    Update Customer.php

--- classess/Customer.php
+++ classess/Customer.php
@@ -195,33 +195,7 @@ $this->updateCustomerOtp($otp,$cmrId);
 	
 }
 
-public function SMS($cellnumber, $otp){
-	
-	
-	$ch = curl_init();
-	$parameters = array(
-    'apikey' => 'fa5f528216e898e9cb9edcc3879e9a83', //Your API KEY
-    'number' => $cellnumber,
-    'message' => 'Here is your OTP code: '.$otp,
-    'sendername' => 'SEMAPHORE'
-);
-curl_setopt( $ch, CURLOPT_URL,'https://semaphore.co/api/v4/messages' );
-curl_setopt( $ch, CURLOPT_POST, 1 );
 
-//Send the parameters set above with the request
-curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $parameters ) );
-
-// Receive response from server
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-$output = curl_exec( $ch );
-curl_close ($ch);
-
-//Show the server response
-//echo $output;
-
-
-
-}
 
 
  
